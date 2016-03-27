1bd11e6f6fd238e87047032d1185d5293dbeae30
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-03-27 21:38:00 +0100

    Test error when arguments of foward_contact are invalid

--- app/models/foward_contact.rb
+++ app/models/foward_contact.rb
@@ -1,3 +1,5 @@
 class FowardContact < ActiveRecord::Base
   has_secure_token :token
+  
+  validates :to, presence: true
 end
--- test/controllers/foward_contacts_controller_test.rb
+++ test/controllers/foward_contacts_controller_test.rb
@@ -12,12 +12,20 @@ class FowardContactsControllerTest < ActionController::TestCase
 
   test 'should create foward_contact' do
     assert_difference('FowardContact.count') do
-      post :create, foward_contact: { fields: @foward_contact.fields, to: @foward_contact.to, token: @foward_contact.token }
+      post :create, foward_contact: { fields: @foward_contact.fields, to: @foward_contact.to }
     end
 
     assert_response :success
     result = JSON.parse(response.body)
     assert_equal 'ok', result['status']
+  end
+  
+  test 'should not create foward_contact with invalid fields' do
+    assert_no_difference('FowardContact.count') do
+      post :create, foward_contact: { fields: '', to: '', token: 'token' }
+    end
+
+    assert_template :new
   end
 
   test 'should deliver email' do
